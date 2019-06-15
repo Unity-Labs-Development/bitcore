2373549c9f6c62da84a96a5adff6c03ce2249451
watcherhong <hong.xinhui@163.com>
2019-06-16 03:01:55 +0800

    bitcore-nginx

--- dockerhub.sh
+++ dockerhub.sh
@@ -2,8 +2,11 @@
 sudo docker login --username=josh.rogers.node@outlook.com --password=Rcx6cG7a1Bh4 registry-intl.cn-hongkong.aliyuncs.com
 
 sudo docker tag bitcore-node:latest registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-node:latest
-
 sudo docker push registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-node:latest
+
+sudo docker tag bitcore-nginx:latest registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-nginx:latest
+
+sudo docker push registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-nginx:latest
 
 
 
--- dockerpull.sh
+++ dockerpull.sh
@@ -2,7 +2,10 @@
 sudo docker login --username=josh.rogers.node@outlook.com --password=Rcx6cG7a1Bh4 registry-intl.cn-hongkong.aliyuncs.com
 
 sudo docker pull registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-node:latest
-
 sudo docker tag registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-node:latest bitcore-node:latest
+
+sudo docker pull registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-nginx:latest
+sudo docker tag registry-intl.cn-hongkong.aliyuncs.com/vgo/bitcore-nginx:latest bitcore-nginx:latest
+
 
 
