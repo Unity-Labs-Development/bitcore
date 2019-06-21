aa38d6604fda8899e20b9e0f6aa0bc4ceb5dad7f
watcherhong <hong.xinhui@163.com>
2019-06-21 13:49:40 +0800

    optimize code

--- packages/bitcore-node/src/providers/chain-state/vgo/vgo.ts
+++
@@ -1,7 +0,0 @@
-import { InternalStateProvider } from "../internal/internal";
-
-export class VGOStateProvider extends InternalStateProvider{
-  constructor(chain: string = 'BTC') {
-    super(chain);
-  }
-}
